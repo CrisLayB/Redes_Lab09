15fa217503280d2db2eec8c49c93b2ccba23a763
CrisLayB <54615310+CrisLayB@users.noreply.github.com>
2023-11-09 21:21:33 -0600

    Kafka implemented and example

---
+++ src/main/java/firstapp/App.java
@@ -0,0 +1,26 @@
+package firstapp;
+
+/**
+ * Universidad del Valle de Guatemala
+ * Facultad de Ingeniería
+ * Departamento de Ciencias de la computación
+ *
+ * Redes - Sección 10
+ * 
+ * Cristian Fernando Laynez Bachez - 201281
+ * Mario David de Leon Muralles - 19019
+ */
+
+/**
+ * Referencias:
+ * https://docs.oracle.com/es-ww/iaas/Content/Streaming/Tasks/streaming-kafka-java-client-quickstart.htm
+ */
+
+
+public class App 
+{    
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
